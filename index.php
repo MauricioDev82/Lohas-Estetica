e319cfbb38157dab7fb55eae82f986ba6220b17c
Mauricio Gomes <mauriciogcosta@uol.com.br>
2021-03-03 14:21:27 -0300

    Versionamento do meu primeiro site para Deploy.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lohas Estética</title>
+</head>
+<body>
+
+<h1>Lohas Estética</h1>
+<p>Para cuidar da beleza da mulher, depilação, limpeza de pele, drenagem linfática</p>
+
+</body>
+</html>
