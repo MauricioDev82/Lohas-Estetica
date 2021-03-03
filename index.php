08ce18ec2ec7b797ec58d960d8a5f2d3ce203728
Mauricio Gomes <mauriciogcosta@uol.com.br>
2021-03-03 15:58:25 -0300

    Trabalhando com cores em HYML.

--- index.php
+++ index.php
@@ -8,5 +8,34 @@
 <h1>Lohas Estética</h1>
 <p>Para cuidar da beleza da mulher, depilação, limpeza de pele, drenagem linfática</p>
 
+<div style="width:100%;display:flex;height: 80px">
+
+<div style="width:50%;backgroud:blue">
+
+</div>
+
+
+<div style="width:50%;backgroud:red">
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
